278c5f0829e610440150a8e27c958531176b239d
dydgjs200 <dydgjs2016@naver.com>
2024-02-13 09:39:09 +0900

    connect git

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 2;
         int b = 3;
-        System.out.println(a+b);
+        int c = 5;
+        System.out.println(a+b+c);
     }
 }
